9d176977b571f97d9164bcf597d468228bb6f93b
Akseli Koskela <playester@gmail.com>
2016-03-29 11:02:59 +0300

    29.3. tunnin hommat

--- SpacePool/GamePage.xaml.cs
+++ SpacePool/GamePage.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +25,108 @@ namespace SpacePool
     /// </summary>
     public sealed partial class GamePage : Page
     {
+        private Player player;
+        private DispatcherTimer timer;
+
+        // location
+        public double LocationX { get; set; }
+        public double LocationY { get; set; }
+
+        // canvas width and height (used to randomize a new flower)
+        private double CanvasWidth;
+        private double CanvasHeight;
+
+        //keys
+        private bool SpacePressed;
+        private bool LeftPressed;
+        private bool RightPressed;
         public GamePage()
         {
             this.InitializeComponent();
+
+            // change the default startup mode
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            // specify the size
+            ApplicationView.PreferredLaunchViewSize = new Size(1280, 720);
+
+
+            // key listeners
+            Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;
+            Window.Current.CoreWindow.KeyUp += CoreWindow_KeyUp;
+
+            // get canvas width and height
+            CanvasWidth = MyCanvas.Width;
+            CanvasHeight = MyCanvas.Height;
+
+            // add player
+            player = new Player
+            {
+                LocationX = CanvasWidth/2,
+                LocationY = CanvasHeight/2
+            
+            };
+            // tähän vielä children tavalla pelaajan lisäys peliin
+            MyCanvas.Children.Add(player);
+
+            player.UpdateLocation();
+
+            // game loop
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 1000 / 60);
+            timer.Start();
         }
+
+        private void CoreWindow_KeyUp(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            switch (args.VirtualKey)
+            {
+                case VirtualKey.Left:
+                    LeftPressed = true;
+                    Debug.WriteLine("Left Pressed");
+                    break;
+                case VirtualKey.Right:
+                    RightPressed = true;
+                    break;
+                case VirtualKey.Space:
+                    SpacePressed = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void CoreWindow_KeyDown(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            switch (args.VirtualKey)
+            {
+                case VirtualKey.Left:
+                    LeftPressed = false;
+                    break;
+                case VirtualKey.Right:
+                    RightPressed = false;
+                    break;
+                case VirtualKey.Space:
+                    SpacePressed = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        private void Timer_Tick(object sender, object e)
+        {
+
+            // move
+
+            if (LeftPressed) player.Move(-1);
+            if (RightPressed) player.Move(1);
+        }
+
+        private void CheckCollision()
+        {
+
+        }
+        
     }
 }
---
+++ SpacePool/Player.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SpacePool
+{
+    public sealed partial class Player : UserControl
+    {
+        
+        
+
+        // location in x
+        public double LocationX { get; set; }
+        public double LocationY { get; set; }
+
+        //speed
+        private readonly double MaxSpeed = 15.0;
+        private readonly double Accelerate = 1.0;
+        private double speed;
+
+
+        public Player()
+        {
+            this.InitializeComponent();
+
+            
+        }
+
+        public void Move( int direction)
+        {
+            //more speed
+            speed += Accelerate;
+            if (speed > MaxSpeed) speed = MaxSpeed;
+            SetValue(Canvas.TopProperty, LocationY);
+            
+
+            // update location values
+            LocationX -= 10 * direction;
+
+            UpdateLocation();
+        }
+        public void UpdateLocation()
+        {
+            SetValue(Canvas.LeftProperty, LocationX);
+            SetValue(Canvas.TopProperty, LocationY);
+        }
+    }
+}
