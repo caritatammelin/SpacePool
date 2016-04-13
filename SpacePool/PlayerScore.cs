fa032d97aeb8e41a0a86f95261682d1878f0695a
yunamarth <kanon93@windowslive.com>
2016-04-13 15:27:01 +0300

    PlayerScore class and Score list

---
+++ SpacePool/PlayerScore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpacePool
+{
+   
+
+    class PlayerScore
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+        public ObservableCollection<PlayerScore> PlayScores { get; }
+
+        public PlayerScore()
+        {
+            PlayScores = new ObservableCollection<PlayerScore>();
+        }
+
+        public void AddScore(PlayerScore score)
+        {
+            PlayScores.Add(score);
+        }
+
+    }
+}
--- SpacePool/ScorePage.xaml.cs
+++ SpacePool/ScorePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -25,6 +26,12 @@ namespace SpacePool
     /// </summary>
     public sealed partial class ScorePage : Page
     {
+        private MediaElement mediaElement;
+        private MediaElement clickElement;
+
+        private ObservableCollection<PlayerScore> scores = new ObservableCollection<PlayerScore>();
+
+ 
     
         public ScorePage()
         {
@@ -34,15 +41,44 @@ namespace SpacePool
                = ApplicationViewWindowingMode.PreferredLaunchViewSize;
             ApplicationView.PreferredLaunchViewSize = new Size(1280, 720);
 
-            XmlDocument xdoc = new XmlDocument();
-            //xdoc.LoadXml();
-            //xdoc.Save("scores.xml");
+            LoadAudio();
 
+           // list of default scores
+            scores.Add(new PlayerScore { Name = "Ajax", Score = 10000 });
+            scores.Add(new PlayerScore { Name = "Angel Dust", Score = 8000 });
+            scores.Add(new PlayerScore { Name = "Colossus", Score = 9500 });
+            scores.Add(new PlayerScore { Name = "Negasonic Teenage Warhead", Score = 8500 });
+
+            
+
+     
+
+
+           // ScoresListView.ItemsSource = Scores;
         }
+
+        public async void LoadAudio()
+        {
+            mediaElement = new MediaElement();
+            mediaElement.AutoPlay = true;
+            mediaElement.IsLooping = true;
+            StorageFolder folder1 = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync("Assets");
+            StorageFile file1 = await folder1.GetFileAsync("spaceambience.wav");
+            var stream1 = await file1.OpenAsync(FileAccessMode.Read);
+            mediaElement.SetSource(stream1, file1.ContentType);
+            clickElement = new MediaElement();
+            clickElement.AutoPlay = false;
+            StorageFolder folder2 = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync("Assets");
+            StorageFile file2 = await folder2.GetFileAsync("click.wav");
+            var stream2 = await file2.OpenAsync(FileAccessMode.Read);
+            clickElement.SetSource(stream2, file2.ContentType);
+        }
+
 
         private void againButton_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(GamePage));
+            clickElement.Play();
         }
 
         private void scoreBlock_SelectionChanged(object sender, RoutedEventArgs e)
@@ -50,13 +86,5 @@ namespace SpacePool
             
         }
 
-        /*public void Main()
-        {
-            var scores = ReadScoresFromFile("Highscores.txt");
-
-            scores.ForEach(s => Console.WriteLine(s));
-
-            Console.ReadKey();
-        }*/
     }
 }
